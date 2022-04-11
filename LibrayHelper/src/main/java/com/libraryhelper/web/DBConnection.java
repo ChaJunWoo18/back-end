8ad4b6237281b4f2b64ae2f13460374de11d4c5a
Janghyen <54027496+Janghyen@users.noreply.github.com>
2022-04-11 21:19:11 +0900

    Add files via upload

---
+++ LibrayHelper/src/main/java/com/libraryhelper/web/DBConnection.java
@@ -0,0 +1,26 @@
+package com.libraryhelper.web;
+
+import java.sql.*;
+
+public class DBConnection {
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	
+	String driver = "com.mysql.jdbc.Driver";
+	String url = "jdbc:mysql://localhost:3306/library_db?serverTimezone=UTC";
+	String uid = "root";
+	String upw = "wkdgus9712";
+	
+	public boolean connection() {
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, uid, upw);
+			System.out.println("connect");
+			return true; // 데이터베이스 접속 성공
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return false; // 데이터베이스 오류
+	}
+}
---
+++ LibrayHelper/src/main/java/com/libraryhelper/web/Login.java
@@ -0,0 +1,26 @@
+package com.libraryhelper.web;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		
+		DBConnection db = new DBConnection();
+		db.connection();
+		
+	}
+}
