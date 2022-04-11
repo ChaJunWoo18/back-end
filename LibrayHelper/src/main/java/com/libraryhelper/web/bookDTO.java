acbe3f49dd004896c7dbc60037b76e2b427d3778
Janghyen <54027496+Janghyen@users.noreply.github.com>
2022-04-11 21:21:28 +0900

    Add files via upload

---
+++ LibrayHelper/src/main/java/com/libraryhelper/web/bookDTO.java
@@ -0,0 +1,36 @@
+package com.libraryhelper.web;
+
+public class bookDTO {
+	private int id = 0;
+	private String book_name = null;
+	private String writer = null;
+	private boolean isMisalign = false;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBook_name() {
+		return book_name;
+	}
+	public void setBook_name(String book_name) {
+		this.book_name = book_name;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public boolean isMisalign() {
+		return isMisalign;
+	}
+	public void setMisalign(boolean isMisalign) {
+		this.isMisalign = isMisalign;
+	}
+	
+	
+}
